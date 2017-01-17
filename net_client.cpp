91fcfd8faa96c935777ea8cfd315e33739807a48
argaon <arttempler@naver.com>
2017-01-17 10:05:01 +0900

    Fix input type cin>>char to input argument

--- net_client.cpp
+++ net_client.cpp
@@ -6,23 +6,19 @@
  
 using namespace std;
 
-int main(){
+int main(int argc, char* argv[]){
     WSADATA wsaData;
     if(WSAStartup(MAKEWORD(2,2), &wsaData) != 0) {
         cout << "원속초기화 실패"<<endl;
         system("pause");
         return 1;
     }		
-	char i_url[100];
+	char *i_url[100];
 	string firstorder ="GET / HTTP/1.1\r\nHost:";
 	string lastorder ="\r\nConnection: close\r\n\r\n";
 	string o_url;
 
-	cout <<"연결할 URL을 입력하세요"<<endl;
-	cout <<"URL 입력 : ";
-	cin >> i_url;
-
-	firstorder.append(i_url);
+	firstorder.append(*i_url);
 	o_url = firstorder.append(lastorder);
 	const char *cstr = o_url.c_str();	
 
@@ -30,7 +26,7 @@ int main(){
 
 	SOCKET Socket=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
     struct hostent *host;
-	host = gethostbyname(i_url);
+	host = gethostbyname(*i_url);
     SOCKADDR_IN SockAddr;
     SockAddr.sin_port=htons(80);
     SockAddr.sin_family=AF_INET;
@@ -41,13 +37,12 @@ int main(){
         system("pause");
         return 1;
     }
-
     cout << "연결되었습니다."<<endl;
 	send(Socket,cstr,strlen(cstr),0);	
 
 	char buffer[100000] = {0};
     int nDataLength = 0;
-   while((nDataLength = recv(Socket,buffer,100000,0)) > 0){
+	while((nDataLength = recv(Socket,buffer,100000,0)) > 0){
 		int i = 0;
 		while(buffer[i] !=0) {		
 		cout << buffer[i];
